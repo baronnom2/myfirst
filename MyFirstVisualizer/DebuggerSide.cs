ee2b1b1729fd9aa5f18faf27c084958df693fce6
baronnom2 <73042968+baronnom2@users.noreply.github.com>
2020-11-17 23:35:54 +0200

    gfgfg

---
+++ MyFirstVisualizer/DebuggerSide.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+
+[assembly: System.Diagnostics.DebuggerVisualizer(
+typeof(MyFirstVisualizer.DebuggerSide),
+typeof(VisualizerObjectSource),
+Target = typeof(System.String),
+Description = "My First Visualizer")]
+
+namespace MyFirstVisualizer
+{
+    public class DebuggerSide: DialogDebuggerVisualizer
+    {
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            MessageBox.Show(objectProvider.GetObject().ToString());
+        }
+        public static void TestShowVisualizer(object objectToVisualize)
+        {
+            VisualizerDevelopmentHost visualizerHost = new VisualizerDevelopmentHost(objectToVisualize, typeof(DebuggerSide));
+            visualizerHost.ShowVisualizer();
+        }
+    }
+}
